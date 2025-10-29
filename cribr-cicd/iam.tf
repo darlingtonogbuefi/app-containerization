83b77156dcb7f2f7283f62a3df2d29935368aa57
Darlington Ogbuefi <ogbuefidarlington@yahoo.com>
2025-10-29 22:20:03 +0000

    Updated buildspec.yaml

--- cribr-cicd/iam.tf
+++ cribr-cicd/iam.tf
@@ -8,23 +8,6 @@
 # CodeBuild Role
 #############################################
 
-resource "aws_iam_role" "codebuild_role" {
-  name = "${var.project_name}-codebuild-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
 resource "aws_iam_role_policy" "codebuild_policy" {
   name = "${var.project_name}-codebuild-policy"
   role = aws_iam_role.codebuild_role.id
@@ -52,23 +35,30 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       },
       # ECR Docker Image Push/Pull
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "ecr:*"
         ]
         Resource = "*"
       },
-      # EKS Access
+      # EKS + AssumeRole
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
-          "eks:*"
-        ]
-        Resource = "*"
+          "eks:*",
+          "sts:AssumeRole"
+        ],
+        "Resource": "*"
+      },
+      # Allow specifically assuming the cribr-cicd-role
+      {
+        Effect = "Allow",
+        Action = "sts:AssumeRole",
+        Resource = "arn:aws:iam::493834426110:role/cribr-cicd-role"
       },
       # Secrets Manager access
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "secretsmanager:*"
         ]
@@ -76,7 +66,7 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       },
       # Deployment Access (ECS, Lambda, CloudFormation)
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "ecs:*",
           "lambda:*",
@@ -86,64 +76,18 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       },
       # IAM PassRole
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = "iam:PassRole"
         Resource = "*"
       },
       # KMS access if secrets or artifacts are encrypted
       {
-        Effect = "Allow"
+        Effect = "Allow",
         Action = [
           "kms:Decrypt",
           "kms:Encrypt",
           "kms:GenerateDataKey",
           "kms:DescribeKey"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-#############################################
-# CodePipeline Role
-#############################################
-resource "aws_iam_role" "codepipeline_role" {
-  name = "${var.project_name}-codepipeline-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "codepipeline.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy" "codepipeline_policy" {
-  name = "${var.project_name}-codepipeline-policy"
-  role = aws_iam_role.codepipeline_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      # Allow Pipeline to Start Builds & Manage Artifacts
-      {
-        Effect = "Allow"
-        Action = [
-          "codebuild:StartBuild",
-          "codebuild:BatchGetBuilds",
-          "codestar-connections:UseConnection",
-          "s3:GetObject",
-          "s3:PutObject",
-          "s3:ListBucket",
-          "iam:PassRole",
-          "cloudwatch:*"
         ]
         Resource = "*"
       }
